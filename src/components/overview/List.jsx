a4224b7ad2d975fea8ad31eca4d6101e0952d8ec
Ahmed <51075069+AhmedNihmet@users.noreply.github.com>
2022-02-23 22:02:20 +0300

    updated the transaction list

--- src/components/overview/List.jsx
+++ src/components/overview/List.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import {
-  Row, Col, Space, Avatar, Button, Modal
+  Row, Col, Space, Avatar, Button, Modal, List as AntList
 } from "antd";
 import {
   IssuesCloseOutlined
 } from "@ant-design/icons";
 
-import Statistics from './Statistics' 
+import Statistics from './Statistics'
 import NewTransactios from './NewTransactios'
 
 export default function List() {
@@ -18,10 +18,18 @@ export default function List() {
   const closeTransactionModal = () => {
     setViewTransactionModal(false)
   }
-  
+
+
+  const listData = [];
+  for (let i = 0; i < 12; i++) {
+    listData.push({
+      title: `ant design part ${i}`,
+    });
+  }
+
   return (
     <>
-    <Modal
+      <Modal
         centered
         footer={false}
         key={Math.random()}
@@ -31,9 +39,9 @@ export default function List() {
         visible={viewTransactionModal}
         onCancel={closeTransactionModal}
       >
-        <NewTransactios 
+        <NewTransactios
           closeModal={closeTransactionModal}
-        /> 
+        />
       </Modal>
 
       <Row gutter={[0, 80]}>
@@ -42,55 +50,38 @@ export default function List() {
         </Col>
         <Col span={24} className="overview_transactions">
           <span>This Week</span>
-          <div className="transaction_container">
 
-            <div>
-              <Space>
-                <Avatar
-                  icon={<IssuesCloseOutlined />}
-                  style={{ backgroundColor: '#1c658c' }}
-                />
-                <p>12 Rules for life by Jordan Peterson signed by himself...</p>
-              </Space>
-              <div className="transaction_row_end_child">
-                <span>Today</span>
-                <div>-$45</div>
+          <AntList
+            itemLayout="vertical"
+            dataSource={listData}
+            className="custom_list"
+            style={{ marginTop: 15 }}
+            pagination={{ pageSize: 6 }}
+            renderItem={(item) => (
+              <div className="transaction_row">
+                <Space>
+                  <Avatar
+                    icon={<IssuesCloseOutlined />}
+                    style={{ backgroundColor: '#1c658c' }}
+                  />
+                  <p>12 Rules for life by Jordan Peterson signed by himself...</p>
+                </Space>
+                <div className="transaction_row_end_child">
+                  <span>Today</span>
+                  <div>-$45</div>
+                </div>
               </div>
-            </div>
-            <div>
-              <Space>
-                <Avatar
-                  icon={<IssuesCloseOutlined />}
-                  style={{ backgroundColor: '#1c658c' }}
-                />
-                <p>12 Rules for life by Jordan Peterson signed by himself...</p>
-              </Space>
-              <div className="transaction_row_end_child">
-                <span>Today</span>
-                <div>-$45</div>
-              </div>
-            </div>
-            <div>
-              <Space>
-                <Avatar
-                  icon={<IssuesCloseOutlined />}
-                  style={{ backgroundColor: '#1c658c' }}
-                />
-                <p>12 Rules for life by Jordan Peterson signed by himself...</p>
-              </Space>
-              <div className="transaction_row_end_child">
-                <span>Today</span>
-                <div>-$45</div>
-              </div>
-            </div>
-
-          </div>
-        </Col>
-        <Col span={24} style={{ textAlign: 'end' }}>
+            )}
+          />
           <Button
             shape='round'
             type='primary'
             onClick={showTransactionModal}
+            style={{ 
+              right: 0,
+              bottom: 0,
+              position: 'absolute'
+            }}
           >
             Add Transaction
           </Button>
