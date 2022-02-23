99bbc90a94e55577883e6c5268087d5b1c292133
Ahmed <51075069+AhmedNihmet@users.noreply.github.com>
2022-02-24 01:13:08 +0300

    added image and colors to transactions

--- src/components/basic/CustomList.jsx
+++ src/components/basic/CustomList.jsx
@@ -1,17 +1,93 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
-  Space, Avatar,  
+  Space, Avatar,
 } from "antd";
-import { IssuesCloseOutlined } from "@ant-design/icons";
+
+// gategory images
+import Loan from '../../assets/icons/svg/loan.svg';
+import Food from '../../assets/icons/svg/food.svg';
+import Gift from '../../assets/icons/svg/gift.svg';
+import Tech from '../../assets/icons/svg/tech.svg';
+import Sports from '../../assets/icons/svg/sport.svg';
+import Health from '../../assets/icons/svg/health.svg';
+import Salary from '../../assets/icons/svg/salary.svg';
 
 export default function CustomList({ item }) {
-  console.log(item)
+  const [avatarSrc, setAvatarSrc] = useState()
+  const [avatarBg, setAvatarBg] = useState('#ffffff')
+
+  const handleAvatar = (item) => {
+    switch (item.category) {
+      case 'Salary':
+        setAvatarSrc(Salary);
+        break;
+      case 'Loan':
+        setAvatarSrc(Loan);;
+        break;
+      case 'Gift':
+        setAvatarSrc(Gift);;
+        break;
+      case 'Tech':
+        setAvatarSrc(Tech);;
+        break;
+      case 'Food':
+        setAvatarSrc(Food);;
+        break;
+      case 'Sports':
+        setAvatarSrc(Sports);;
+        break;
+      case 'Health':
+        setAvatarSrc(Health);;
+        break;
+
+      default:
+      // do nothing
+    }
+  }
+
+  const handleAvatarBg = (item) => {
+    switch (item.category) {
+      case 'Salary':
+        setAvatarBg('#faad14');
+        break;
+      case 'Loan':
+        setAvatarBg('#bae637');
+        break;
+      case 'Gift':
+        setAvatarBg('#d4b106');
+        break;
+      case 'Tech':
+        setAvatarBg('#ff7875');
+        break;
+      case 'Food':
+        setAvatarBg('#73d13d');
+        break;
+      case 'Sports':
+        setAvatarBg('#13c2c2');
+        break;
+      case 'Health':
+        setAvatarBg('#9254de');
+        break;
+
+      default:
+      // do nothing
+    }
+  }
+
+  useEffect(() => {
+    handleAvatar(item);
+    handleAvatarBg(item)
+  }, [item])
+
   return (
     <div className="transaction_row">
       <Space>
         <Avatar
-          icon={<IssuesCloseOutlined />}
-          style={{ backgroundColor: '#1c658c' }}
+          className="transaction_img"
+          src={(
+            <img src={avatarSrc} alt="" />
+          )}
+          style={{ backgroundColor: avatarBg }}
         />
         <p>12 Rules for life by Jordan Peterson signed by himself...</p>
       </Space>
