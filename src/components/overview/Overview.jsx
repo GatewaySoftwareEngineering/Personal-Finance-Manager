fba2dcdae55d0a656f3c7b351b90633839461daf
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2022-02-28 23:15:42 +0300

    add: overview cards

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -1,7 +1,45 @@
 import React from "react";
-
+import OverviewCard from "./OverviewCard";
+const cards = [
+  {
+    title: "income",
+    value: 1000,
+    bg: "from-sky-300 via-sky-200 to-sky-100",
+    textColor: "text-sky-600",
+    detailBg: "bg-sky-400",
+  },
+  {
+    title: "balance",
+    value: 4500,
+    bg: "from-zinc-300 via-zinc-200 to-zinc-100",
+    textColor: "text-zinc-600",
+    detailBg: "bg-zinc-400",
+  },
+  {
+    title: "expense",
+    value: 1500,
+    bg: "from-rose-300 via-rose-200 to-rose-100",
+    textColor: "text-rose-600",
+    detailBg: "bg-rose-400",
+  },
+];
 const Overview = () => {
-  return <div className="h-full bg-yellow-500">Overview</div>;
+  return (
+    <div className="h-full p-6">
+      <div className="flex gap-8">
+        {cards.map((card, index) => (
+          <OverviewCard
+            key={index}
+            title={card.title}
+            value={card.value}
+            bg={card.bg}
+            textColor={card.textColor}
+            detailBg={card.detailBg}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Overview;
---
+++ src/components/overview/OverviewCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const OverviewCard = ({ title, value, bg, textColor, detailBg }) => {
+  return (
+    <div className={`h-28 w-72 rounded-lg bg-gradient-to-r ${bg} py-5 px-7`}>
+      <div className="flex items-center justify-between">
+        <h4 className={`capitalize ${textColor}`}>{title}</h4>
+        <button
+          className={`text-white ${detailBg} h-5 w-16 rounded-xl bg-opacity-75 text-xs hover:underline`}
+        >
+          details
+        </button>
+      </div>
+      <p className="mt-3 text-3xl text-white">
+        {"$"}
+        {value}
+      </p>
+    </div>
+  );
+};
+
+export default OverviewCard;
