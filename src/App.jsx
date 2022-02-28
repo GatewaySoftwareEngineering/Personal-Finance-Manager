30750374205211ecb6405ad6d4ee75fff7a6c1f8
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2022-02-28 17:16:35 +0300

    configure: routes of the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Overview from "./components/overview/Overview";
+import SideBar from "./components/side-bar/SideBar";
+import TopBar from "./components/top-bar/TopBar";
+import TransactionHistory from "./components/transaction-history/TransactionHistory";
 
 function App() {
-  return <div className="container text-22">App</div>;
+  return (
+    <div className="container text-22">
+      <SideBar />
+      <TopBar />
+      <Routes>
+        <Route path="/" element={<Overview />} />
+        <Route path="/transaction-history" element={<TransactionHistory />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
