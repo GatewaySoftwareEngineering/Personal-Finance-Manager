a464117be4bf5e73e908f833b58acf261eea106b
Ibrahem ahmad <53955363+ibrahemahmad@users.noreply.github.com>
2022-07-25 00:52:41 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
+import { fetchTransactions } from "./features/transactions/transactionsSlice";
 import OverView from "./pages/overview";
 import Transactions from "./pages/transactions";
 
 function App() {
+  const dispatch=useDispatch()
+  useEffect(()=>{
+    dispatch(fetchTransactions());
+  })
   return (
     <div>
       <Routes>
