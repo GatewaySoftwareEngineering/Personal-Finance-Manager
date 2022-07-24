74f44c923857875a6bd3d453b65ed20d05490703
Ibrahem ahmad <53955363+ibrahemahmad@users.noreply.github.com>
2022-07-25 00:51:44 +0300

    config

---
+++ src/configs/constants.js
@@ -0,0 +1,13 @@
+export const typeIncome = {
+  SALARY: "SALARY",
+  LOAN: "LOAN",
+  GIFT: "GIFT",
+};
+export const typeExpence = {
+  TECH: "TECH",
+  FOOD: "FOOD",
+  BILLS: "BILLS",
+  SPORTS: "SPORTS",
+  HEALTH: "HEALTH",
+};
+export const allCategory={...typeIncome,...typeExpence}
